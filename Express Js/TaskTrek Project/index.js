6f67210dfb992b6d546eba6ae8313b6518dda45e
nesarikartejas10 <nesarikartejas10@gmail.com>
2025-12-22 08:18:41 +0530

    create new todo

--- Express Js/TaskTrek Project/index.js
+++ Express Js/TaskTrek Project/index.js
@@ -25,6 +25,8 @@ const todos = [
   },
 ];
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Welcome to TaskTrek Project!..");
 });
@@ -40,6 +42,21 @@ app.get("/todos/:id", (req, res) => {
 
   const todo = todos.find((todo) => todo.id === todoId);
   res.send(todo);
+});
+
+//Create a new todo
+app.post("/todos", (req, res) => {
+  const { task, tags, status } = req.body;
+
+  const newTodo = {
+    id: todos[todos.length - 1].id + 1,
+    task,
+    tags,
+    status,
+  };
+
+  todos.push(newTodo);
+  res.send(newTodo);
 });
 
 const PORT = process.env.PORT || 3000;
