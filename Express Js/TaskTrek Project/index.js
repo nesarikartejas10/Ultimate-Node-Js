fd9473f95242abdc23f1a8a79b65a9408546f754
nesarikartejas10 <nesarikartejas10@gmail.com>
2025-12-22 06:40:28 +0530

    fetch all todos api

--- Express Js/TaskTrek Project/index.js
+++ Express Js/TaskTrek Project/index.js
@@ -2,8 +2,34 @@ import express from "express";
 
 const app = express();
 
+const todos = [
+  {
+    id: 1,
+    task: "Create all APIs for Project 01",
+    tags: ["NodeJS", "JavaScript"],
+    status: "todo",
+  },
+  {
+    id: 2,
+    task: "Create API for list of all Todos",
+    tags: ["NodeJS"],
+    status: "doing",
+  },
+  {
+    id: 3,
+    task: "Plan Project 01",
+    tags: ["JavaScript"],
+    status: "done",
+  },
+];
+
 app.get("/", (req, res) => {
   res.send("Welcome to TaskTrek Project!..");
+});
+
+//fetch all todos
+app.get("/todos", (req, res) => {
+  res.send(todos);
 });
 
 app.listen(3000, () => {
