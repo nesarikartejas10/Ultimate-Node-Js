63218de9efbc3671fee9076b42ffadb5f78b89f7
nesarikartejas10 <nesarikartejas10@gmail.com>
2025-12-24 19:47:33 +0530

    async await

---
+++ Express Js/Asynchronous JS/async-await.js
@@ -0,0 +1,43 @@
+function fetchStudentData(studId) {
+  return new Promise((resolve, reject) =>
+    setTimeout(() => {
+      console.log("Fetching student data from database...");
+      if (studId) {
+        resolve({
+          id: studId,
+          name: "Tejas",
+          enrollNo: 5951,
+        });
+      } else {
+        reject(
+          new Error("Error occurs fetching student data from database...")
+        );
+      }
+    }, 3000)
+  );
+}
+
+function getResult(enrollment) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Fetching student result from database...");
+      if (enrollment) {
+        resolve({ resultId: enrollment, percentage: 70 });
+      } else {
+        reject(new Error("Error occurs fetching student result"));
+      }
+    }, 2000);
+  });
+}
+
+async function printResult() {
+  try {
+    const student = await fetchStudentData(51);
+    const result = await getResult(student.enrollNo);
+    console.log("Result", result);
+  } catch (error) {
+    console.log("error", error);
+  }
+}
+
+printResult();
