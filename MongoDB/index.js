974aca96c4244d951cd18efa6c311825f251eaa3
nesarikartejas10 <nesarikartejas10@gmail.com>
2025-12-25 12:56:17 +0530

    define schema in mongodb

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -1,6 +1,17 @@
 import mongoose from "mongoose";
 
+//connection
 mongoose
   .connect("mongodb://localhost:27017/mongo-demo")
   .then(() => console.log("MongoDB connect successfully!!"))
   .catch((error) => console.log("MongoDB connection failed!!", error));
+
+//define schema
+new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, unique: true, lowercase: true },
+  password: { type: String, required: true },
+  phone: { type: Number },
+  hobbies: { type: [String] },
+  isVerified: { type: Boolean, default: false },
+});
