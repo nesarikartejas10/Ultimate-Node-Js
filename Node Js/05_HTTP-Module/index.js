f3f32425ec4e993ba74edffc7bea10e44ac1339b
nesarikartejas10 <nesarikartejas10@gmail.com>
2025-12-26 09:34:26 +0530

    http module serve json and html

--- Node Js/05_HTTP-Module/index.js
+++ Node Js/05_HTTP-Module/index.js
@@ -1,14 +1,30 @@
 const http = require("node:http");
+const fs = require("node:fs");
 
+//send json
+// const server = http.createServer((req, res) => {
+//   if (req.url === "/") {
+//     res.end("serve json");
+//   } else if (req.url === "/api") {
+//     const data = { name: "Tejas", age: 31, success: true };
+//     res.writeHead(200, { "Content-Type": "application/json" });
+//     res.end(JSON.stringify(data));
+//   } else {
+//     res.writeHead(404, { "Content-Type": "text/plain" });
+//     res.end("Not Found");
+//   }
+// });
+
+//send html
 const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.write("Welcome to Home page");
-  } else if (req.url === "/about") {
-    res.write("This is about page");
+  if (req.url === "/html") {
+    const html = fs.readFileSync("index.html", "utf-8");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(html);
   } else {
-    res.write("Route not found");
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Not Found");
   }
-  res.end();
 });
 
 server.listen(3000, () => {
