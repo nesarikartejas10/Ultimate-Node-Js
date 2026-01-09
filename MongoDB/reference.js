b3e04faf33a2563b8ada000f2bd7846bb027cd4c
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-01-09 08:09:48 +0530

    apply reference relationship in mongodb

---
+++ MongoDB/reference.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+mongoose
+  .connect("mongodb://localhost:27017/mongo-relationship")
+  .then(() => console.log("MongoDB connected Successfully!!"))
+  .catch((err) => console.log("MongoDB Connection Failed", err));
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number,
+});
+
+const postSchema = new mongoose.Schema({
+  content: String,
+  date: { type: Date, default: Date.now },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+});
+
+const User = mongoose.model("User", userSchema);
+const Post = mongoose.model("Post", postSchema);
+
+async function createUser(name, email, age) {
+  const newUser = new User({
+    name: name,
+    email: email,
+    age: age,
+  });
+
+  await newUser.save();
+  console.log(newUser);
+}
+
+async function createPost(content, user) {
+  const newPost = new Post({
+    content: content,
+    user: user,
+  });
+
+  await newPost.save();
+  console.log(newPost);
+}
+
+async function getPosts() {
+  const posts = await Post.find();
+  console.log(posts);
+}
+
+// createUser("Tejas", "code@gmail.com", 25);
+
+createPost("Content 2", "696068b640cfac98d6a68f79");
+
+// getPosts();
