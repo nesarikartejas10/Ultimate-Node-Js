9701ef5881c9d2ef498be3b96918b131c6d28570
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-01-10 11:37:29 +0530

    validate user data with joi

--- Cartwish Backend/src/routes/users.js
+++ Cartwish Backend/src/routes/users.js
@@ -1,12 +1,28 @@
 import express from "express";
 import User from "../model/users.js";
 import bcrypt from "bcrypt";
+import Joi from "joi";
 
 const router = express.Router();
+
+const createUserSchema = Joi.object({
+  name: Joi.string().min(3).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+  deliveryAddress: Joi.string().min(5).required(),
+});
 
 //Register Route
 router.post("/", async (req, res) => {
   const { name, email, password, deliveryAddress } = req.body;
+
+  const joiValidation = createUserSchema.validate(req.body);
+
+  if (joiValidation.error) {
+    return res.status(400).json(joiValidation.error.details[0].message);
+  }
+
+  // res.json(joiValidation);
 
   const user = await User.findOne({ email: email });
 
