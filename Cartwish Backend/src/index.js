d1664732366a0263d3303ea7b271e09d6aeb5662
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-01-10 06:25:17 +0530

    connect app to mongodb

--- Cartwish Backend/src/index.js
+++ Cartwish Backend/src/index.js
@@ -1,8 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
+//setup express server
 const app = express();
+
+//mongodb connection
+mongoose
+  .connect("mongodb://localhost:27017/cartwish")
+  .then(() => console.log("MongoDB connect successfully!!"))
+  .catch((error) => console.log("MongoDB connection failed!!", error));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
