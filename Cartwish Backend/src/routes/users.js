ea29ec4d8ddd969dd71f341c69e30eae254415b9
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-01-11 06:54:24 +0530

    create login route

--- Cartwish Backend/src/routes/users.js
+++ Cartwish Backend/src/routes/users.js
@@ -46,15 +46,40 @@ router.post("/", async (req, res) => {
 
   await newUser.save();
 
-  const token = jwt.sign(
-    { _id: newUser._id, name: newUser.name },
-    process.env.JWT_KEY,
-    { expiresIn: "2h" }
-  );
+  const token = generateToken({ _id: newUser._id, name: newUser.name });
 
   res
     .status(201)
     .json({ message: "User register successfully.", newUser, token: token });
 });
+
+//login route
+router.post("/login", async (req, res) => {
+  //find the user from database by email
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email: email });
+  if (!user) {
+    return res.status(401).json({ message: "Invalid Credentials." });
+  }
+  //compare encrypted password
+  const validPassword = await bcrypt.compare(password, user.password);
+  if (!validPassword) {
+    return res.status(401).json({ message: "Invalid Credentials." });
+  }
+  //create jwt token and send it in response
+  const token = generateToken({ _id: user._id, name: user.name });
+
+  res
+    .status(201)
+    .json({ message: "User login successfully.", user, token: token });
+});
+
+//generate jwt token
+function generateToken(data) {
+  return jwt.sign(data, process.env.JWT_KEY, {
+    expiresIn: "2h",
+  });
+}
 
 export default router;
