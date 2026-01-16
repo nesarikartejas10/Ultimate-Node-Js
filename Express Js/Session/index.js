b0e59bf27edbf288c69cff1f13d5a57f85fca81f
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-01-16 07:10:44 +0530

    change collection name sessions to mySessions

--- Express Js/Session/index.js
+++ Express Js/Session/index.js
@@ -13,6 +13,7 @@ app.use(
     //store session in mongodb instead of ram
     store: Mongostore.create({
       mongoUrl: "mongodb://127.0.0.1:27017/sessionDb",
+      collectionName: "mySessions",
     }),
   })
 );
